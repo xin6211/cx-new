93f76e0f919aed19d4f028c3af84194fb58525d2
xin6211 <3365638834@qq.com>
2024-07-11 18:55:30 +0800

    rm ;

--- src/cube.tsx
+++ src/cube.tsx
@@ -7,7 +7,7 @@ function Cube() {
             <Title text="Genesis NFT - Axon Cube" />
             <div className="flex flex-row items-center border border-black rounded-regular bg-gridBgImg bg-norepeat bg-bggray h-[42rem] pl-12 overflow-hidden drop-shadow-cardShadow ph:hidden">
                 <div className="pb-24">
-                    <div className="bg-bgpurple p-[2px] font-MonoMedium text-2xl text-black inline-block py-1.5 px-2 rounded-md mb-12">Axon Cube</div>;
+                    <div className="bg-bgpurple p-[2px] font-MonoMedium text-2xl text-black inline-block py-1.5 px-2 rounded-md mb-12">Axon Cube</div>
                     <div className="font-Mono text-black w-[60%] pad:w-[90%]">
                         Holders of Axon Cube can enjoy higher data inclusion rate for their Axon Edge as a data contributor, meanwhile, enjoy exclusive rewards and premium access to future versions
                         and products of CogniXphere.
